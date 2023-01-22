eb03c837377f129a786b2839d384cdbba7c21428
Dennis355 <103638322+Dennis355@users.noreply.github.com>
2023-01-22 22:41:41 +0000

    Deploying to gh-pages from @ Dennis355/goit-js-hw-09@4348b9980a7f722464cf7b2d1fcc41b1d539b98c 🚀

---
+++ 03-promises.0410280b.js
@@ -0,0 +1,2 @@
+function e(e,t){return new Promise(((o,n)=>{const l=Math.random()>.3;setTimeout((()=>{l?o({position:e,delay:t}):n({position:e,delay:t})}),t)}))}document.querySelector(".form").addEventListener("submit",(function(t){t.preventDefault();const o=t.currentTarget.elements;let n=o.delay.value,l=(o.step.value,o.amount.value);for(let t=0;t<=l;t++)e(t,n).then((({position:e,delay:t})=>{console.log(`✅ Fulfilled promise ${e} in ${t}ms`)})).catch((({position:e,delay:t})=>{console.log(`❌ Rejected promise ${e} in ${t}ms`)}))}));
+//# sourceMappingURL=03-promises.0410280b.js.map
