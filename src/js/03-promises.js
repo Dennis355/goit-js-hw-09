0d0e2744b359b1153d09de185709813d7892f30e
Dennis355 <Snidevichden@gmail.com>
2023-01-24 00:26:11 +0200

    Update 03-promises.js
    fixed

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -23,14 +23,15 @@ function createPromise(position, delay)  {
          function onFormSubmit  (e) {
         e.preventDefault();
         const formEl = e.currentTarget.elements;
-        let delay = formEl.delay.value;
-        let delayStep= formEl.step.value;
-        let amount = formEl.amount.value;
+        let delay = Number(formEl.delay.value);
+        let delayStep = Number(formEl.step.value);
+        console.log('delayStep', delayStep);
+        let amount = Number(formEl.amount.value);
   
       
 
 for (let i = 0; i <= amount; i++) {
-    // delay += delayStep;
+    
     createPromise(i, delay).then (({ position, delay }) => {
         console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
 //   console.log(result);
@@ -39,6 +40,7 @@ for (let i = 0; i <= amount; i++) {
 
     }
     );
-    
+    delay = delay + delayStep;
  };
+
 };
